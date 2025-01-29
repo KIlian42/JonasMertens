0218e8f76ea203fe10a0bec944e7052a5df4fb5b
KIlian42 <57774167+KIlian42@users.noreply.github.com>
2025-01-29 10:01:30 +0100

    Add zoom library

--- src/components/Home_component/Home_component.vue
+++ src/components/Home_component/Home_component.vue
@@ -1,99 +1,119 @@
 <template>
-  <div
-    ref="container"
-    class="map-container"
-    @wheel.prevent="onWheel"
-    @mousedown="onMouseDown"
-    @dblclick="onDoubleClick"
-  >
-    <div ref="content" class="map-content" :style="mapStyle">
-      <div class="box" style="top: 100px; left: 100px"></div>
-      <div class="box" style="top: 300px; left: 200px"></div>
-      <div class="box" style="top: 500px; left: 400px"></div>
-    </div>
+  <div ref="container" class="map-wrapper">
+    <svg ref="svg" class="map-container"></svg>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { onMounted, ref, onBeforeUnmount, nextTick } from 'vue'
+import * as d3 from 'd3'
 
-const scale = ref(1)
-const translateX = ref(0)
-const translateY = ref(0)
-const isDragging = ref(false)
-const lastX = ref(0)
-const lastY = ref(0)
+const container = ref<HTMLDivElement | null>(null)
+const svg = ref<SVGElement | null>(null)
+const width = ref(0)
+const height = ref(0)
 
-const container = ref<HTMLElement | null>(null)
-const content = ref<HTMLElement | null>(null)
-
-const mapStyle = computed(() => ({
-  transform: `translate(${translateX.value}px, ${translateY.value}px) scale(${scale.value})`,
-  transformOrigin: '0 0',
-}))
-
-const onWheel = (event: WheelEvent) => {
-  const scaleFactor = 1.1
-  const rect = container.value!.getBoundingClientRect()
-  const mouseX = event.clientX - rect.left
-  const mouseY = event.clientY - rect.top
-
-  const prevScale = scale.value
-  scale.value = event.deltaY < 0 ? scale.value * scaleFactor : scale.value / scaleFactor
-
-  const deltaScale = scale.value / prevScale
-  translateX.value = mouseX - (mouseX - translateX.value) * deltaScale
-  translateY.value = mouseY - (mouseY - translateY.value) * deltaScale
+const updateSize = () => {
+  if (container.value) {
+    width.value = container.value.clientWidth
+    height.value = container.value.clientHeight
+    d3.select(svg.value).attr('width', width.value).attr('height', height.value)
+  }
 }
 
-const onMouseDown = (event: MouseEvent) => {
-  isDragging.value = true
-  lastX.value = event.clientX
-  lastY.value = event.clientY
-  window.addEventListener('mousemove', onMouseMove)
-  window.addEventListener('mouseup', onMouseUp)
+const onZoom = (
+  container: d3.Selection<SVGGElement, unknown, null, undefined>,
+  event: d3.D3ZoomEvent<SVGElement, unknown>,
+) => {
+  container.attr('transform', event.transform)
 }
 
-const onMouseMove = (event: MouseEvent) => {
-  if (!isDragging.value) return
-  translateX.value += event.clientX - lastX.value
-  translateY.value += event.clientY - lastY.value
-  lastX.value = event.clientX
-  lastY.value = event.clientY
+const initView = (ref: SVGElement | null) => {
+  if (!ref) return
+
+  const svg = d3.select(ref)
+  const container = svg.append('g').classed('container', true)
+
+  // Draw three red rectangles
+  container
+    .append('rect')
+    .attr('x', 100)
+    .attr('y', 100)
+    .attr('width', 100)
+    .attr('height', 100)
+    .style('fill', 'red')
+
+  container
+    .append('rect')
+    .attr('x', 300)
+    .attr('y', 200)
+    .attr('width', 100)
+    .attr('height', 100)
+    .style('fill', 'red')
+
+  container
+    .append('rect')
+    .attr('x', 500)
+    .attr('y', 400)
+    .attr('width', 100)
+    .attr('height', 100)
+    .style('fill', 'red')
+
+  // Init D3 zoom with smooth inertia effect
+  const zoom = d3
+    .zoom<SVGElement, unknown>()
+    .scaleExtent([0.166, 30])
+    .on('zoom', (event) => onZoom(container, event))
+    .filter((event) => {
+      return !(event.type === 'wheel' && event.ctrlKey) // Prevent default zoom on pinch gestures
+    })
+
+  let lastDeltaY = 0
+  svg.call(zoom).on('wheel', (event) => {
+    event.preventDefault()
+    const targetScale = d3.zoomTransform(svg.node()!).k * (event.deltaY < 0 ? 1.1 : 0.9)
+    lastDeltaY = event.deltaY
+    d3.transition()
+      .duration(300)
+      .ease(d3.easeCubicOut)
+      .tween('zoom', () => {
+        const i = d3.interpolate(d3.zoomTransform(svg.node()!).k, targetScale)
+        return (t) => {
+          svg.call(zoom.scaleTo, i(t))
+        }
+      })
+  })
+
+  // Center container within SVG
+  const centered = d3.zoomIdentity.translate(width.value / 2, height.value / 2)
+  svg.call(zoom.transform, centered)
 }
 
-const onMouseUp = () => {
-  isDragging.value = false
-  window.removeEventListener('mousemove', onMouseMove)
-  window.removeEventListener('mouseup', onMouseUp)
-}
+onMounted(() => {
+  nextTick(() => {
+    updateSize()
+    initView(svg.value)
+    window.addEventListener('resize', updateSize)
+  })
+})
 
-const onDoubleClick = (event: MouseEvent) => {
-  const rect = container.value!.getBoundingClientRect()
-  translateX.value = event.clientX - rect.left - content.value!.clientWidth / 2
-  translateY.value = event.clientY - rect.top - content.value!.clientHeight / 2
-}
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateSize)
+})
 </script>
 
 <style>
+.map-wrapper {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
 .map-container {
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-  position: relative;
-  background: white;
-}
-.map-content {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform-origin: 0 0;
-  transition: transform 0.1s ease-out;
-}
-.box {
-  width: 100px;
-  height: 100px;
-  background: red;
-  position: absolute;
+  background-color: #ccc;
+  width: 100%;
+  height: 100%;
 }
 </style>
