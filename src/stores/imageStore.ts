81d9c51c97cc3d6d67b71a02a72cf38d4862c0c2
KIlian42 <57774167+KIlian42@users.noreply.github.com>
2025-01-31 22:32:57 +0100

    Fix reference image public fodler bug

--- src/stores/imageStore.ts
+++ src/stores/imageStore.ts
@@ -3,9 +3,9 @@ import { defineStore } from 'pinia'
 export const useImageStore = defineStore('image', {
   state: () => ({
     images: [
-      { x: 100, y: 100, width: 100, height: 100, src: 'public/11.png' },
-      { x: 300, y: 200, width: 100, height: 100, src: 'public/11.png' },
-      { x: 500, y: 400, width: 100, height: 100, src: 'public/11.png' },
+      { x: 100, y: 100, width: 100, height: 100, src: '/11.png' },
+      { x: 300, y: 200, width: 100, height: 100, src: '/11.png' },
+      { x: 500, y: 400, width: 100, height: 100, src: '/11.png' },
     ],
   }),
   getters: {
