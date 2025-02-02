5d3b08f45638a4083c7630e2d9770fe381cf7c69
KIlian42 <57774167+KIlian42@users.noreply.github.com>
2025-02-02 18:58:08 +0100

    Change resize to 1150px

--- src/components/Main_component/Main_component.ts
+++ src/components/Main_component/Main_component.ts
@@ -4,7 +4,7 @@ export function getViewClass() {
   const isSmallScreen = ref(false)
 
   const checkScreenSize = () => {
-    isSmallScreen.value = window.matchMedia('(max-width: 1150px)').matches
+    isSmallScreen.value = window.matchMedia('(max-width: 1200px)').matches
   }
 
   onMounted(() => {
