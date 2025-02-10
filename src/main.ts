ef8f34c6183f924bd0046d31f712dfaa8020ceee
KIlian42 <57774167+KIlian42@users.noreply.github.com>
2025-02-10 02:41:50 +0100

    Add persistent pinia

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -22,8 +23,9 @@ const vuetify = createVuetify({
 })
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPersist)
+app.use(pinia)
 app.use(router)
 app.use(vuetify)
 
